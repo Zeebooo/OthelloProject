bdf37453ff5c3324e4f44b654d61f94bd35de933
sebbe <sebastian.gabrielsson03@gmail.com>
2025-12-08 11:21:25 +0100

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace OthelloProject.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
